a49efa6e233cc58372c708732d75f10dce2132e9
raflyqowi <raflycow10@gmail.com>
2024-01-18 18:34:50 +0700

    training pipeline completed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from MonkeypoxClassifier import logger
 from MonkeypoxClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from MonkeypoxClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from MonkeypoxClassifier.pipeline.stage_03_training import ModelTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -14,11 +15,24 @@ except Exception as e:
     logger.exception(e)
     raise e
 
+
 STAGE_NAME = "Prepare Base Models"
 
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Training"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = ModelTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
